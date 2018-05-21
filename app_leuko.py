b95410595c5cc6101baa2dbb6db26aafec12845d
minoru789 <33167644+minoru789@users.noreply.github.com>
2018-05-21 18:10:41 -0300

    Update app_leuko.py

--- app_leuko.py
+++ app_leuko.py
@@ -4,10 +4,8 @@ import numpy as np
 import json 
 from flask import Flask
 import os
-from config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
 from app import routes
 from app import lib
